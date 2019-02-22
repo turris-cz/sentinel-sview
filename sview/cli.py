2368b6147518d33213d393950a1e13480503b5a0
Robin Obůrka <robin.oburka@nic.cz>
2019-02-22 09:31:26 +0100

    cli: Add check-redis command
    I want to have a estimate about occupying space in redis.

--- sview/cli.py
+++ sview/cli.py
@@ -6,7 +6,7 @@ from flask.cli import with_appcontext
 from .jobs import load_data_from_template
 from .queries import QUERIES
 
-from .extensions import db
+from .extensions import db, redis
 
 
 @click.command()
@@ -33,9 +33,46 @@ def queue_queries():
         load_data_from_template.queue(k)
 
 
+@click.command()
+@with_appcontext
+def check_redis():
+    """Check state of data in redis"""
+    size = 0
+    available = []
+    missing = []
+
+    for k in QUERIES.keys():
+        data = redis.get(k)
+        if data:
+            available.append(k)
+            size += len(data)
+        else:
+            missing.append(k)
+
+    if available:
+        click.echo("Stored keys:")
+        for k in available:
+            click.echo("  - {}".format(k))
+    if missing:
+        click.echo("Missing keys:")
+        for k in missing:
+            click.echo("  - {}".format(k))
+    if available:
+        click.echo("")
+        click.echo("Keys occupy:  {}".format(_human_readable_bytes(size)))
+
+
+def _human_readable_bytes(size):
+    from math import log2
+    _suffixes = ['bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+    order = int(log2(size) / 10) if size else 0
+    return '{:.4g} {}'.format(size / (1 << (order * 10)), _suffixes[order])
+
+
 def register_cli_commands(app):
     app.cli.add_command(clear_cache)
     app.cli.add_command(queue_queries)
+    app.cli.add_command(check_redis)
 
 
 def register_shell_context(app):
