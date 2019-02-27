77bf0bdce79a27afc47bbc6cf1cd833db98ca62c
Robin Obůrka <robin.oburka@nic.cz>
2019-02-27 13:50:32 +0100

    jobs: Move imports to prevent cyclic dependencies

--- sview/jobs.py
+++ sview/jobs.py
@@ -1,11 +1,11 @@
 from .extensions import rq
-from .data_helpers import get_and_store
 
 from .queries import QUERIES
 
 
 @rq.job(result_ttl=10)
 def load_data_from_template(resource_name):
+    from .data_helpers import get_and_store
     get_and_store(resource_name,
                   QUERIES[resource_name]["query"],
                   params=QUERIES[resource_name].get("params"),
