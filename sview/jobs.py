b7ff8755df742f4fdc780adc1546e8077d303271
Robin Obůrka <robin.oburka@nic.cz>
2019-02-27 13:53:13 +0100

    jobs: Add proof-of-concept mechanism to run awaitable jobs

--- sview/jobs.py
+++ sview/jobs.py
@@ -1,4 +1,4 @@
-from .extensions import rq
+from .extensions import redis, rq
 
 from .queries import QUERIES
 
@@ -10,3 +10,22 @@ def load_data_from_template(resource_name):
                   QUERIES[resource_name]["query"],
                   params=QUERIES[resource_name].get("params"),
                   post_process=QUERIES[resource_name].get("post_process"))
+
+
+def run_job(handler, job_function,  **kwargs):
+    def _queue_job(job_function, **kw):
+        job = job_function.queue(**kw)
+        redis.set(handler, job.id, ex=job.result_ttl)
+        return job.id
+
+    job_id = redis.get(handler)
+    if job_id:
+        job_id = job_id.decode("UTF-8")
+    else:
+        return None, _queue_job(job_function, **kwargs)
+
+    job = rq.get_queue().fetch_job(job_id)
+    if not job:
+        return None, _queue_job(job_function, **kwargs)
+
+    return job.result, None
