e360d93dcf92972fe4d0facd9b593fb8dab3949b
Robin Obůrka <robin.oburka@nic.cz>
2019-02-27 14:06:49 +0100

    Add proof-of-concept implementation of attacker's detail page
    Known problems:
    - Needs refactoring of JS usage (I want it more general)
    - Missing rate limit
    - Not tested corner cases

--- sview/attackers.py
+++ sview/attackers.py
@@ -1,6 +1,9 @@
-from flask import render_template
+from flask import request, render_template, jsonify
 from flask import Blueprint
 
+from .extensions import rq
+
+from .jobs import run_job, attacker_detail
 from .data_helpers import get_data
 
 
@@ -19,3 +22,27 @@ def index():
     data = {k: get_data(k) for k in data_keys}
 
     return render_template("attackers/home.html", **data)
+
+
+@attackers.route("/details/ip/<string:ip>")
+def ip_details(ip):
+    data, await_job = run_job("attackers:details:{}".format(ip), attacker_detail, ip=ip)
+
+    return render_template("attackers/detail.html", await_job=await_job, ip=ip, data=data)
+
+
+@attackers.route("/await", methods=["POST"])
+def ip_details_wait():
+    job_id = request.form.get("job_id")
+    job = rq.get_queue().fetch_job(job_id)
+
+    if not job:
+        return jsonify({"error": "Waiting for nonexistent job"}), 404
+
+    if job.is_failed:
+        return jsonify({"error": "Server error during request processing"}), 500
+
+    if job.is_finished:
+        return jsonify({"job_done": True})
+    else:
+        return jsonify({"job_done": False})
