2bdda5711b1e53190f78c8f45db7d49a2bf09341
Robin Obůrka <robin.oburka@nic.cz>
2019-03-04 11:40:26 +0100

    jobs: Move imports to the top
    - It prevents some duplicities in code in future
    - There is no more chance to have circular imports there

--- sview/jobs.py
+++ sview/jobs.py
@@ -1,11 +1,14 @@
 from .extensions import rq
 
+from .data_helpers import get_and_store, process_query
+from .job_helpers import check_and_mark_data_with_found
+
 from .queries import QUERIES
+from .queries import _attacker_activity_during_time, _attacker_passwords
 
 
 @rq.job(result_ttl=10)
 def load_data_from_template(resource_name):
-    from .data_helpers import get_and_store
     get_and_store(resource_name,
                   QUERIES[resource_name]["query"],
                   params=QUERIES[resource_name].get("params"),
@@ -14,10 +17,6 @@ def load_data_from_template(resource_name):
 
 @rq.job(result_ttl=300)
 def attacker_detail(**kwargs):
-    from .data_helpers import process_query
-    from .job_helpers import check_and_mark_data_with_found
-    from .queries import _attacker_activity_during_time, _attacker_passwords
-
     res = {
         "attacker_activity": process_query(_attacker_activity_during_time, params=kwargs),
         "attacker_passwords": process_query(_attacker_passwords, params=kwargs),
