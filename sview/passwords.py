805dda23cf01c45eed57c74f91e594f6e9b413ca
Robin Obůrka <robin.oburka@nic.cz>
2019-03-04 14:19:56 +0100

    Add experimental data view: password details

--- sview/passwords.py
+++ sview/passwords.py
@@ -1,7 +1,11 @@
+import base64
+
 from flask import render_template
 from flask import Blueprint
 
 from .data_helpers import get_data
+from .job_helpers import run_job, common_await_view
+from .jobs import password_detail
 
 
 passwords = Blueprint("passwords", __name__)
@@ -19,3 +23,22 @@ def index():
     data = {k: get_data(k) for k in data_keys}
 
     return render_template("passwords/home.html", **data)
+
+
+@passwords.route("/details/<string:encoded_password>")
+def password_details(encoded_password):
+    password = str(base64.b64decode(encoded_password), "UTF-8")
+
+    data, job_id = run_job("passwords:details:{}".format(encoded_password),
+                           password_detail,
+                           password=password)
+    if data:
+        if not data["found"]:
+            return render_template("passwords/not_found.html", password=password)
+
+    return render_template("passwords/detail.html", job_id=job_id, password=password, data=data)
+
+
+@passwords.route("/await", methods=["POST"])
+def password_details_wait():
+    return common_await_view()
