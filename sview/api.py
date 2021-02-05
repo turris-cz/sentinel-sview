058ebbb67ff86df239f5938b0020e4b9c1df076c
Martin Prudek <martin.prudek@nic.cz>
2021-02-05 16:03:32 +0100

    Expose resource API
    This API is a preparation for dynamic data reloading
    when switching between different periods or resource parameters.
    
    The server response is a JSON that may contain the following items:
    
        `error` - present only in case of error
                - contains a string with further information
    
        `resource_name` = name of requested/returned resource
    
        `data` = object with various format (only end use components know)
               - equals to `None`/`null` when the data is not ready yet
               - equals to empty `Array`/`List` when no data from choosen period
                 are available

--- sview/api.py
+++ sview/api.py
@@ -1,5 +1,9 @@
 from flask import Blueprint
+from flask import jsonify
+from flask import request
 
+from .exceptions import ResourceError
+from .resources import get_resource
 from .job_helpers import common_await_view
 
 api = Blueprint("api", __name__)
@@ -8,3 +12,20 @@ api = Blueprint("api", __name__)
 @api.route("/job/await", methods=["POST"])
 def await_job():
     return common_await_view()
+
+
+@api.route("/resource")
+def get_resource_view():
+    resource_name = request.args.get("name")
+    if resource_name is None:
+        return jsonify({"error": "No resource name privided."})
+
+    period = request.args.get("period", "1y")
+
+    try:
+        return jsonify({
+            "resource_name": resource_name,
+            "data": get_resource(resource_name, period)
+        })
+    except ResourceError as exc:
+        return jsonify({"error": str(exc)})
