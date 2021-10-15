e9ee54779d991395bce3081fca7b43d2e3287422
Martin Prudek <martin.prudek@nic.cz>
2021-10-15 15:38:51 +0200

    sql: Convert incident queries from FLUX to SQL

--- sview/queries/sql/incidents.py
+++ sview/queries/sql/incidents.py
@@ -1,298 +1,341 @@
+# Graph of all incidents
 all_incidents_graph = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="incident_count")
-        |> group()
-        |> window(every: {window})
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        COUNT(*) as count
+    FROM incidents
+    WHERE
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY day
+    ORDER BY day
 """
 
+# List of top incident types by incidents
 top_incident_types = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="incident_count")
-        |> group(columns:["_field", "action"])
-        |> sum()
-        |> group()
-        |> sort(columns: ["_value"], desc: true)
-        |> rename(columns: {{"_field": "source", "_value": "count"}})
-        |> keep(columns: ["source", "action", "count"])
-        |> limit(n: {limit})
+    SELECT
+        count(*) as count,
+        trap as source,
+        action
+    FROM incidents
+    WHERE
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY trap, action
+    ORDER BY count DESC
+    LIMIT :limit
 """
 
+# Graph of top traps by incidents
 incidents_by_source_trends = """
-    top_sources=from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="incident_count")
-        |> group(columns: ["_field"])
-        |> sum()
-        |> group()
-        |> sort(desc:true)
-        |> limit(n: {top_n})
-        |> findColumn(fn: (key)=>true, column: "_field")
-
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="incident_count"
-            and contains(value: r._field, set:top_sources)
-            )
-        |> group()
-        |> window(every: {window})
-        |> group(columns: ["_field", "_start"])
-        |> sum()
-        |> group(columns: ["_start"])
-        |> rename(columns: {{"_value":"count", "_field": "source"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count", "source"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        trap as source,
+        COUNT(ip) as count
+    FROM incidents
+    WHERE
+        trap IN (
+            SELECT
+                trap
+            FROM (
+                SELECT
+                    trap,
+                    COUNT(*) AS count_inner
+                FROM incidents
+                WHERE
+                    trap IS NOT NULL
+                    AND
+                    now() - INTERVAL :interval < time AND time < now()
+                GROUP BY trap
+                ORDER BY count_inner DESC
+                LIMIT :limit
+            ) AS foo
+        )
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY day, trap
+    ORDER BY day
 """
 
-
+# Graph of top actions by incidents
 incidents_by_action_trends = """
-    top_actions=from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="incident_count" and exists r.action)
-        |> group(columns: ["action"])
-        |> sum()
-        |> group()
-        |> sort(desc:true)
-        |> limit(n: {top_n})
-        |> findColumn(fn: (key)=>true, column: "action")
-
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="incident_count"
-            and exists r.action
-            and contains(value: r.action, set:top_actions)
-            )
-        |> group()
-        |> window(every: {window})
-        |> group(columns: ["action", "_start"])
-        |> sum()
-        |> group(columns: ["_start"])
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count", "action"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        action,
+        COUNT(ip) as count
+    FROM incidents
+    WHERE
+        action IN (
+            SELECT
+                action
+            FROM (
+                SELECT
+                    action,
+                    COUNT(*) AS count_inner
+                FROM incidents
+                WHERE
+                    action IS NOT NULL
+                    AND
+                    now() - INTERVAL :interval < time AND time < now()
+                GROUP BY action
+                ORDER BY count_inner DESC
+                LIMIT :limit
+            ) AS foo
+        )
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY day, action
+    ORDER BY day
 """
+
+# List of top attackers by incidents
 attacker_ips = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count")
-        |> group(columns:["_field"])
-        |> sum()
-        |> rename(columns: {{"_field":"ip", "_value":"count"}})
-        |> keep(columns: ["ip", "count"])
-        |> group()
-        |> sort(columns: ["count"], desc: true)
-        |> limit(n: {limit})
+    SELECT
+        ip,
+        count(ip) AS count
+    FROM incidents
+    WHERE
+        ip IS NOT NULL
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY ip
+    ORDER BY count DESC
+    LIMIT :limit
 """
 
+# Graph of all attackers
 attackers_by_day = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count")
-        |> group()
-        |> window(every: {window})
-        |> unique(column: "_field")
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count"])
-        |> group()
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        COUNT(DISTINCT ip) as count
+    FROM incidents
+    WHERE
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY day
+    ORDER BY day
 """
 
+# Graph of selected attacker's incidents
 attackers_activity_graph = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count" and r._field=="{ip}")
-        |> group()
-        |> window(every: {window})
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count"])
-        |> group()
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        COUNT(*) as count
+    FROM incidents
+    WHERE
+        ip = :ip
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY day
+    ORDER BY day
 """
 
+# List of top countries by incidents
 top_countries_by_incidents_table = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count" and exists r.country)
-        |> group(columns:["country"])
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> keep(columns: ["country", "count"])
-        |> group()
-        |> sort(columns: ["count"], desc: true)
-        |> limit(n: {limit})
+    SELECT
+        country,
+        count(country) AS count
+    FROM incidents
+    WHERE
+        country IS NOT NULL
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY country
+    ORDER BY count DESC
+    LIMIT :limit
 """
 
+# List of top countries by attackers
 top_countries_by_attackers_table = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count" and exists r.country)
-        |> group(columns:["country"])
-        |> unique(column: "_field")
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> keep(columns: ["country", "count"])
-        |> group()
-        |> sort(columns: ["count"], desc: true)
-        |> limit(n: {limit})
+    SELECT
+        country,
+        count(distinct(ip)) AS count
+    FROM incidents
+    WHERE
+        ip IS NOT NULL
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY country
+    ORDER BY count DESC
+    LIMIT :limit
 """
 
+# List of all countries by incidents
 incidents_map = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count" and exists r.country)
-        |> group(columns:["country"])
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> keep(columns: ["country", "count"])
-        |> group()
+    SELECT
+        country,
+        count(*) AS count
+    FROM incidents
+    WHERE
+        country IS NOT NULL
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+    GROUP BY country
 """
 
+# List of all countries by attackers
 attackers_map = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="attacker_incident_count" and exists r.country)
-        |> group(columns:["country"])
-        |> unique(column: "_field")
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> keep(columns: ["country", "count"])
-        |> group()
+    SELECT
+        country,
+        count(distinct(ip)) AS count
+    FROM incidents
+    WHERE
+        country IS NOT NULL
+        AND
+        now() - INTERVAL :interval < time AND time < now()
+        AND
+        ip is not null
+    GROUP BY country
 """
 
+# Graph of top countries by attackers
 top_countries_trends = """
-    top_countries=from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="attacker_incident_count" and exists r.country)
-        |> group(columns: ["country"])
-        |> unique(column: "_field")
-        |> sum()
-        |> group()
-        |> sort(desc:true)
-        |> limit(n: {top_n})
-        |> findColumn(fn: (key)=>true, column: "country")
-
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="attacker_incident_count"
-            and exists r.country
-            and contains(value: r.country, set:top_countries)
-            )
-        |> group()
-        |> window(every: {window})
-        |> unique(column: "_field")
-        |> group(columns: ["country", "_start"])
-        |> sum()
-        |> group(columns: ["_start"])
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count", "country"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        country,
+        COUNT(DISTINCT ip) as count
+    FROM incidents
+    WHERE
+        now() - INTERVAL :interval < time AND time < now()
+        AND country IN (
+            SELECT
+                country
+            FROM (
+                SELECT
+                    country,
+                    COUNT(DISTINCT(ip)) AS count_inner
+                FROM incidents
+                WHERE
+                    now() - INTERVAL :interval < time AND time < now()
+                    AND
+                    country IS NOT NULL
+                GROUP BY country
+                ORDER BY count_inner DESC
+                LIMIT :limit
+            ) AS foo
+        )
+    GROUP BY day, country
+    ORDER BY day
 """
 
+# Graph of top countries by incidents
 incidents_by_country_trends = """
-    top_countries=from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="attacker_incident_count" and exists r.country)
-        |> group(columns: ["country"])
-        |> sum()
-        |> group()
-        |> sort(desc:true)
-        |> limit(n: {top_n})
-        |> findColumn(fn: (key)=>true, column: "country")
-
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="attacker_incident_count"
-            and exists r.country
-            and contains(value: r.country, set:top_countries)
-            )
-        |> group()
-        |> window(every: {window})
-        |> group(columns: ["country", "_start"])
-        |> sum()
-        |> group(columns: ["_start"])
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count", "country"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        country,
+        COUNT(*) as count
+    FROM incidents
+    WHERE
+        now() - INTERVAL :interval < time AND time < now()
+        AND
+        country IN (
+            SELECT
+                country
+            FROM (
+                SELECT
+                    country,
+                    COUNT(*) AS count_inner
+                FROM incidents
+                WHERE
+                    now() - INTERVAL :interval < time AND time < now()
+                    AND
+                    country IS NOT NULL
+                GROUP BY country
+                ORDER BY count_inner DESC
+                LIMIT :limit
+            ) AS foo
+        )
+    GROUP BY day, country
+    ORDER BY day
 """
+
+# Graph of top countries by incidents reported by selected devices
 my_incidents_by_country_trends = """
-    top_countries=from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="user_incident_count"
-            and exists r.country
-            and contains(value: r._field, set: {my_device_tokens})
-            )
-        |> group(columns: ["country"])
-        |> sum()
-        |> group()
-        |> sort(desc:true)
-        |> limit(n: {top_n})
-        |> findColumn(fn: (key)=>true, column: "country")
-
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="user_incident_count"
-            and exists r.country
-            and contains(value: r.country, set:top_countries)
-            and contains(value: r._field, set: {my_device_tokens})
-            )
-        |> group()
-        |> window(every: {window})
-        |> group(columns: ["country", "_start"])
-        |> sum()
-        |> group(columns: ["_start"])
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count", "country"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        incidents.country AS country,
+        COUNT(*) as count
+    FROM incidents, identity
+    WHERE
+        now() - INTERVAL :interval < incidents.time AND incidents.time < now()
+        AND
+        identity.device_token in :my_device_tokens
+        AND
+        incidents.identity_id=identity.id
+        AND
+        incidents.country IN (
+            SELECT
+                country_inner
+            FROM (
+                SELECT
+                    incidents.country as country_inner,
+                    COUNT(*) AS count_inner
+                FROM incidents, identity
+                WHERE
+                    now() - INTERVAL :interval < incidents.time AND incidents.time < now()
+                    AND
+                    incidents.country IS NOT NULL
+                    AND
+                    incidents.identity_id=identity.id
+                    AND
+                    identity.device_token in :my_device_tokens
+                GROUP BY country_inner
+                ORDER BY count_inner DESC
+                LIMIT :limit
+            ) AS top_countries
+        )
+    GROUP BY day, country
+    ORDER BY day
 """
 
+# Graph of top traps by incidents reported by selected devices
 my_incidents_by_source_trends = """
-    top_sources=from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="user_incident_count"
-            and contains(value: r._field, set: {my_device_tokens})
-            and exists r.source
-            )
-        |> group(columns: ["source"])
-        |> sum()
-        |> group()
-        |> sort(desc:true)
-        |> limit(n: {top_n})
-        |> findColumn(fn: (key)=>true, column: "source")
-
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r)=>r._measurement=="user_incident_count"
-            and contains(value: r.source, set:top_sources)
-            and contains(value: r._field, set: {my_device_tokens})
-            )
-        |> group()
-        |> window(every: {window})
-        |> group(columns: ["source", "_start"])
-        |> sum()
-        |> group(columns: ["_start"])
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count", "source"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        incidents.trap AS source,
+        COUNT(*) as count
+    FROM incidents, identity
+    WHERE
+        now() - INTERVAL :interval < incidents.time AND incidents.time < now()
+        AND
+        identity.device_token in :my_device_tokens
+        AND
+        incidents.identity_id=identity.id
+        AND
+        incidents.trap IN (
+            SELECT
+                trap_inner
+            FROM (
+                SELECT
+                    incidents.trap as trap_inner,
+                    COUNT(*) AS count_inner
+                FROM incidents, identity
+                WHERE
+                    now() - INTERVAL :interval < incidents.time AND incidents.time < now()
+                    AND
+                    incidents.trap IS NOT NULL
+                    AND
+                    incidents.identity_id=identity.id
+                    AND
+                    identity.device_token in :my_device_tokens
+                GROUP BY trap_inner
+                ORDER BY count_inner DESC
+                LIMIT :limit
+            ) AS top_traps
+        )
+    GROUP BY day, source
+    ORDER BY day
 """
 
+# Graph of all incidents reported by selected devices
 my_incidents_graph = """
-    from(bucket: "{bucket}")
-        |> range(start: {start})
-        |> filter(fn: (r) =>r._measurement=="user_incident_count"
-            and contains(value: r._field, set: {my_device_tokens})
-            )
-        |> group()
-        |> window(every: {window})
-        |> sum()
-        |> rename(columns: {{"_value":"count"}})
-        |> map(fn:(r) => ({{ r with day: string(v: r._start) }}))
-        |> keep(columns: ["day", "count"])
+    SELECT
+        to_char(time_bucket(:bucket, time), 'YYYY-MM-DD HH24:MI') AS day,
+        COUNT(*) as count
+    FROM incidents, identity
+    WHERE
+        now() - INTERVAL :interval < incidents.time AND incidents.time < now()
+        AND
+        incidents.identity_id=identity.id
+        AND
+        identity.device_token in :my_device_tokens
+    GROUP BY day
+    ORDER BY day
 """
-
