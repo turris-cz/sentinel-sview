b816ba3de54679f7cf55ded4f5b2588d2f1b112e
Aleksandr Gumroian <aleksandr.gumroian@nic.cz>
2022-01-20 18:06:52 +0300

    Add page title to dynfw page

--- sview/dynfw.py
+++ sview/dynfw.py
@@ -6,8 +6,13 @@ dynfw = Blueprint("dynfw", __name__)
 
 @dynfw.route("/dynfw/")
 def index():
+    page_title = "Dynamic Firewall"
     data = {
         "server": current_app.config["DYNFW_SOCKET_ADDR"],
         "port": current_app.config["DYNFW_SOCKET_PORT"],
     }
-    return render_template("dynfw/dynfw.html", dynfwhost=data)
+    return render_template(
+        "dynfw/dynfw.html",
+        dynfwhost=data,
+        page_title=page_title
+    )
